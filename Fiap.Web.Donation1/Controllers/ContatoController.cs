38c8a78c1210dae8f71ce54750ee38ddb6cb039c
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-09-12 22:00:34 -0300

    alterações da aula 01

---
+++ Fiap.Web.Donation1/Controllers/ContatoController.cs
@@ -0,0 +1,29 @@
+﻿using Fiap.Web.Donation1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation1.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gravar(ContatoModel contatoModel)
+        {
+            return View("Sucesso");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Fiap.Web.Donation1/Models/ContatoModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Fiap.Web.Donation1.Models
+{
+    public class ContatoModel
+    {
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set;}
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+    }
+}
