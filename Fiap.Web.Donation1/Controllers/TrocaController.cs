f0cfafb6df6b62e546538c2b9cc7846c54d1ed3f
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-09-26 23:06:33 -0300

    refatorando as regras de trocas

--- Fiap.Web.Donation1/Controllers/TrocaController.cs
+++ Fiap.Web.Donation1/Controllers/TrocaController.cs
@@ -1,6 +1,7 @@
 ﻿using Fiap.Web.Donation1.Data;
 using Fiap.Web.Donation1.Migrations;
 using Fiap.Web.Donation1.Repository;
+using Fiap.Web.Donation1.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -12,12 +13,15 @@ namespace Fiap.Web.Donation1.Controllers
 
         private readonly TrocaRepository trocaRepository;
 
+        private readonly TrocaService trocaService;
+
         private readonly int UsuarioId = 1;
 
         public TrocaController(DataContext dataContext)
         {
             produtoRepository = new ProdutoRepository(dataContext);
             trocaRepository = new TrocaRepository(dataContext);
+            trocaService = new TrocaService(produtoRepository, trocaRepository);
         }
 
 
@@ -38,44 +42,22 @@ namespace Fiap.Web.Donation1.Controllers
         [HttpPost]
         public IActionResult Index(Models.TrocaModel trocaModel)
         {
-            var produto1 = produtoRepository.FindById(trocaModel.ProdutoId1);
-            var produto2 = produtoRepository.FindById(trocaModel.ProdutoId2);
 
             try
             {
-                if (produto1.Disponivel == false)
-                {
-                    throw new Exception("Produto selecionado indisponível");
-                }
+                trocaService.TrocarProdutos(trocaModel);
 
-                if (produto2.Disponivel == false)
-                {
-                    throw new Exception("O seu produto já foi trocado");
-                }
-
-                if ((produto2.Valor / produto1.Valor) < 0.9)
-                {
-                    throw new Exception("O seu produto tem o valor menor que 90% do produto selecionado");
-                }
-
-                produto1.Disponivel = false;
-                produtoRepository.Update(produto1);
-
-                produto2.Disponivel = false;
-                produtoRepository.Update(produto2);
-
-                trocaModel.Status = Models.TrocaStatus.Iniciado;
-                trocaRepository.Insert(trocaModel);
-
-                return RedirectToAction("Index", "Home");
-
-            } catch (Exception ex)
-            {
-                ViewBag.Erro = ex.Message;
-                ComboMeusProdutos();
-                return View(trocaModel);
+                TempData["Mensagem"] = "Troca registrada com sucesso.";
             }
+            catch (Exception ex)
+            {
+                TempData["Erro"] = ex.Message;
+            }
+
+            return RedirectToAction("Index", "Home");
         }
+
+        
 
 
 
---
+++ Fiap.Web.Donation1/Services/TrocaService.cs
@@ -0,0 +1,49 @@
+﻿using Fiap.Web.Donation1.Repository;
+
+namespace Fiap.Web.Donation1.Services
+{
+    public class TrocaService
+    {
+
+        private readonly ProdutoRepository produtoRepository;
+
+        private readonly TrocaRepository trocaRepository;
+
+        public TrocaService(ProdutoRepository _produtoRepository, TrocaRepository _trocaRepository)
+        {
+            produtoRepository = _produtoRepository;
+            trocaRepository = _trocaRepository;
+        }
+
+        public void TrocarProdutos(Models.TrocaModel trocaModel)
+        {
+            var produto1 = produtoRepository.FindById(trocaModel.ProdutoId1);
+            var produto2 = produtoRepository.FindById(trocaModel.ProdutoId2);
+
+            if (produto1.Disponivel == false)
+            {
+                throw new Exception("Produto selecionado indisponível");
+            }
+
+            if (produto2.Disponivel == false)
+            {
+                throw new Exception("O seu produto já foi trocado");
+            }
+
+            if ((produto2.Valor / produto1.Valor) < 0.9)
+            {
+                throw new Exception("O seu produto tem o valor menor que 90% do produto selecionado");
+            }
+
+            produto1.Disponivel = false;
+            produtoRepository.Update(produto1);
+
+            produto2.Disponivel = false;
+            produtoRepository.Update(produto2);
+
+            trocaModel.Status = Models.TrocaStatus.Iniciado;
+            trocaRepository.Insert(trocaModel);
+        }
+
+    }
+}
